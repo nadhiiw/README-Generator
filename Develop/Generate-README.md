13a0e342495d6422779a4511e8709e1992e68520
Nadhi Woliye <nwoliye@gmail.com>
2021-04-14 00:02:47 -0500

    make change to reame and add generate readme.md

---
+++ Develop/Generate-README.md
@@ -0,0 +1,53 @@
+# readme
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/Nadhiiw/undefined?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/Nadhiiw/undefined?style=flat&logo=appveyor)
+  
+  Check out the badges hosted by [shields.io](https://shields.io/).
+  
+  
+  ## Description 
+  
+  *The what, why, and how:* 
+  
+  generater
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  
+  ## Installation
+  
+  *Steps required to install project and how to get the development environment running:*
+  
+  steps
+  
+  ## Usage 
+  
+  *Instructions and examples for use:*
+
+  
+  steps
+  
+  ## Tests
+  
+  *Tests for application and how to run them:*
+  
+  steps
+  
+  ## License
+  
+  MIT
+  
+  ---
+  
+  ## Questions?
+  
+  ![Developer Profile Picture](undefined) 
+  
+  For any questions, please contact me with the information below:
+ 
+  GitHub: [@undefined](undefined)
+  
+  Email: undefined
+  
--- Develop/index.js
+++ Develop/index.js
@@ -1,74 +1,118 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const util = require('util');
+
+const api = require('./utils/api');
+const markdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-    type: 'input',
-    name: 'username',
-    message: 'what is your github username?',
-
-    validate: function (answer) {
-        if (answer.length < 1){
-            return console.log('You must enter your github username.');
+        type: 'input',
+        name: 'username',
+        message: 'what is your github username?',
+        default: 'connietran-dev',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log('You must enter your github username.');
+            }
+            return true;
         }
-        return true;
-    }
-},
-{
-    type: 'input',
-    name: 'repository',
-    message: 'What is your github repository?',
-
-    validate: function (answer) {
-        if (answer.length < 1){
-          return  console.log('You must enter your github repository.');
+    },
+    {
+        type: 'input',
+        name: 'repository',
+        message: 'What is your github repository?',
+        default: 'readme-generator',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log('You must enter your github repository.');
+            }
+            return true;
         }
-        return true;
-    }
-}, 
+    },
 
-{
-    type: 'input',
-    name: 'title',
-    message: 'What is your project title?',
-   
-    validate: function (answer) {
-        if (answer.length < 1){
-          return  console.log('You must enter your github repository.');
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log('You must enter your github repository.');
+            }
+            return true;
         }
-        return true;
-    }
-}, 
+    },
 
-{
-    type: 'input',
-    name: 'description',
-    message: 'Enter the description of your project.', 
-
-    validate: function (answer){
-        if (answer.length < 1){
-            return console.log('you must enter the description of your project.')
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter the description of your project.',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log('you must enter the description of your project.')
+            }
+            return true;
         }
-        return true;
-    }
-}
+    },
+    {
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Provide instructions and examples of your project in use for the Usage section.",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['MIT', 'Apache', 'GPLv2', 'none'],
+        name: 'license'
+    },
 
 
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => if(err){
-       return console.log(err);
-    }
-    console.log('Your markdown file has been created.')
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Your markdown file has been created")
+    });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+
+// Main function
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            // console.log(answers);
+            var readme = markdown(answers, api.getUser(answers.username));
+            // console.log(readme);
+            writeToFile('Generate-README.md', readme);
+        });
+}
 
 // Function call to initialize app
 init();
